529f9319c6ab1b22628d1a29830142ef309137ae
Ch Saqib <chsaqib11223@gmail.com>
2025-01-27 18:06:19 +0500

    feat: add SidebarMenu component with accordion and scroll area functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarMenu } from "@/components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex">
+          <div className="hidden sm:block">
+            <SidebarMenu />
+          </div>
+          <main className="w-full p-4">{children}</main>
+        </div>
       </body>
     </html>
   );
