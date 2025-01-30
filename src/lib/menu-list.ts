0f58800c816586d55aad1b27a9171f90f35329ef
Ch Saqib <chsaqib11223@gmail.com>
2025-01-31 00:56:00 +0500

    feat: update menu list to include History group with new submenu items

--- src/lib/menu-list.ts
+++ src/lib/menu-list.ts
@@ -1,101 +1,91 @@
 import {
-    Tag,
-    Users,
-    Settings,
-    Bookmark,
-    SquarePen,
-    LayoutGrid,
-    LucideIcon,
-    Bot
-  } from "lucide-react";
-  
-  type Submenu = {
-    href: string;
-    label: string;
-    active?: boolean;
-  };
-  
-  type Menu = {
-    href: string;
-    label: string;
-    active?: boolean;
-    icon: LucideIcon;
-    submenus?: Submenu[];
-  };
-  
-  type Group = {
-    groupLabel: string;
-    menus: Menu[];
-  };
-  
-  export function getMenuList(pathname: string): Group[] {
-    return [
-      {
-        groupLabel: "",
-        menus: [
-          {
-            href: "/",
-            label: "Home",
-            icon: LayoutGrid,
-            submenus: []
-          }
-        ]
-      },
-      {
-        groupLabel: "",
-        menus: [
-          {
-            href: "/chat",
-            label: "Chat",
-            icon: Bot,
-            submenus: []
-          }
-        ]
-      },
-      {
-        groupLabel: "Contents",
-        menus: [
-          {
-            href: "",
-            label: "Posts",
-            icon: SquarePen,
-            submenus: [
-              {
-                href: "/posts",
-                label: "All Posts"
-              },
-              {
-                href: "/posts/new",
-                label: "New Post"
-              }
-            ]
-          },
-          {
-            href: "/categories",
-            label: "Categories",
-            icon: Bookmark
-          },
-          {
-            href: "/tags",
-            label: "Tags",
-            icon: Tag
-          }
-        ]
-      },
-      {
-        groupLabel: "Settings",
-        menus: [
-          {
-            href: "/users",
-            label: "Users",
-            icon: Users
-          },
-          {
-            href: "/account",
-            label: "Account",
-            icon: Settings
-          }
-        ]
-      }
-    ];
-  }
+  Tag,
+  Users,
+  Settings,
+  Bookmark,
+  SquarePen,
+  LayoutGrid,
+  LucideIcon,
+  Bot,
+} from "lucide-react";
+
+type Submenu = {
+  href: string;
+  label: string;
+  active?: boolean;
+};
+
+type Menu = {
+  href: string;
+  label: string;
+  active?: boolean;
+  icon: LucideIcon;
+  submenus?: Submenu[];
+};
+
+type Group = {
+  groupLabel: string;
+  menus: Menu[];
+};
+
+export function getMenuList(pathname: string): Group[] {
+  return [
+    {
+      groupLabel: "",
+      menus: [
+        {
+          href: "/",
+          label: "Home",
+          icon: LayoutGrid,
+          submenus: [],
+        },
+      ],
+    },
+    {
+      groupLabel: "",
+      menus: [
+        {
+          href: "/chat",
+          label: "Chat",
+          icon: Bot,
+          submenus: [],
+        },
+      ],
+    },
+    {
+      groupLabel: "History",
+      menus: [
+        {
+          href: "",
+          label: "Posts",
+          icon: SquarePen,
+          submenus: [
+            {
+              href: "/",
+              label: "All History",
+            },
+            {
+              href: "/",
+              label: "New Chat",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      groupLabel: "Settings",
+      menus: [
+        {
+          href: "/users",
+          label: "Users",
+          icon: Users,
+        },
+        {
+          href: "/account",
+          label: "Account",
+          icon: Settings,
+        },
+      ],
+    },
+  ];
+}
